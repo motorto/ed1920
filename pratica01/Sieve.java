4b61c75f263d6f83d8c829805ccd3ac4d4783236
André Cerqueira <acerqueira021@protonmail.com>
2020-02-17 20:34:53 +0000

    aula 1

---
+++ pratica01/Sieve.java
@@ -0,0 +1,34 @@
+public class Sieve {
+
+    // Procedimento para usar o algoritmo do Crivo de Eratóstenes
+    // [procedimento ainda por completar]
+    public static void sieve(int n, boolean prime[]) {
+		for(int i=0;i<=n;i++){
+			prime[i]=true;
+		}
+		 //int i=1;
+		for(int j=2;j<=n;j++){  // <--- Multiplicador (K) 
+			for (int i=2;i<=n;i++){		// <-- Avancar o numero pelo qual multiplica (Ki) 
+				if((j*i)>n){
+					break;
+				}
+				else
+					prime[j*i]=false;
+				
+			}
+		}
+    }
+    
+    public static void main(String[] args) {
+	
+	int n=100; 
+	
+	boolean[] prime = new boolean[n+1];
+
+	sieve(n, prime);
+
+	for (int i=2; i<=n; i++)
+	    if (prime[i])
+		System.out.println(i);
+    }
+}
