2230bab0543825fc9c53bd5fce4ec367dc7ff49d
André Cerqueira <acerqueira021@protonmail.com>
2020-02-25 18:35:57 +0000

    .

---
+++ pratica01/ED120.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ED120{ 
+
+        static void losango(int n){
+                int espacos=n/2;
+                int cardinais=1;
+                for (int j=0;j<n/2+1;j++){
+                        for(int i=0;i<espacos;i++){
+                                System.out.print(".");
+                        }
+                        for(int i=0;i<cardinais;i++){
+                                System.out.print("#");
+                        }
+                        for(int i=0;i<espacos;i++){
+                                System.out.print(".");
+                        }
+                        System.out.println();
+                        espacos--;
+                        cardinais+=2;
+                } 
+                cardinais-=2;
+                espacos++;
+                for(int j=0;j<n/2;j++){
+                        espacos++;
+                        cardinais-=2;
+                        for(int i=0;i<espacos;i++){
+                                System.out.print(".");
+                        }
+                        for(int i=0;i<cardinais;i++){
+                                System.out.print("#");
+                        }
+                        for(int i=0;i<espacos;i++){
+                                System.out.print(".");
+                        }
+                        System.out.println();
+                }
+
+                
+        }
+
+        public static void main (String[] args){
+                Scanner ler = new Scanner(System.in);
+                int size = ler.nextInt();
+                losango(size);
+        }
+
+}
+
---
+++ pratica02/ED121.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ED121{
+
+        static boolean palindromo (String str){
+                int size = str.length() - 1;
+                for(int i=0 ;i<size;i++){
+                        if (str.charAt(i) != str.charAt(size))
+                                return false;
+                        size-- ;
+                }
+                return true; 
+        }
+
+        public static void main (String[] args){
+                Scanner ler = new Scanner(System.in);
+                int number = ler.nextInt();
+                for (int i=0 ; i< number ; i++){
+                        String str = ler.nextLine();
+                        if (palindromo(str))
+                                System.out.println("sim");
+                        else 
+                                System.out.println("nao");
+                }
+        }
+
+}
